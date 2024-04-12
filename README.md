52129d8c55ed289ec0da0bffa2142e760fe4089f
Yuchen He <yuchenhe126@gmail.com>
2024-04-12 14:50:41 +0200

    flake-config: Init public flake
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

---
+++ README.md
@@ -0,0 +1,12 @@
+# Nix(OS) flake configuration files
+
+## Installation
+
+Create, format and mount needed partitions.
+
+Generate `hardware-configuration.nix` in `./host/<host>`
+
+```
+cd <repo>
+$ nixos-install --flake .#<host>
+```
---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "My really flaky flake";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;}
+    {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+
+      imports = [./hosts];
+    };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  };
+}
